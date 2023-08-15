eb07dca419c472ca24fe7a4e962682e6dc1c2fa8
Mazhar Hussain <realmazharhussain@gmail.com>
2023-08-15 14:20:22 +0500

    Use custom_target() for generating POT file

--- po/update-pot-file.sh
+++ po/update-pot-file.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 po_dir=$(dirname "$(realpath "$0")")
 
+if test -z "$PACKAGE_VERSION" || test -z "$SOURCE_ROOT"; then
+    echo "Do not run this script directly. Instead, run" \
+         "'meson compile -C <buiddir> pot_file'." >&2
+    exit 1
+fi
+
+cd "$SOURCE_ROOT"
+
 options=(
   -f "$po_dir"/POTFILES.in
   -o "$po_dir"/daikhan.pot
@@ -10,7 +18,7 @@ options=(
   --from-code=UTF-8
 
   --package-name="daikhan"
-  --package-version="pre-alpha"
+  --package-version="$PACKAGE_VERSION"
   --msgid-bugs-address="realmazharhussain@gmail.com"
 )
 
